dae6bbda85d9af9936296b8423ab4b81fe12ed6f
Muhammad Nouman Attique <108257688+kingpin147@users.noreply.github.com>
2024-01-15 14:48:09 +0500

    postgress update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
-import Image from 'next/image'
+const getData = async () => {
+  try {
+    const response = await fetch('https://localhost:3000/api/users');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const res = await response.json();
+    return res;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    // Handle the error gracefully, e.g., display an error message
+    return []; // Return an empty array in case of an error
+  }
+};
 
-export default function Home() {
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
+
+  // Check if data has elements before accessing properties
+  const userData = data && data.length > 0 ? data[0] : { username: '', email: '' };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Hello
+      <p>{`aik tera ${userData.username}, aik mera ${userData.email}`}</p>
     </main>
-  )
+  );
 }
